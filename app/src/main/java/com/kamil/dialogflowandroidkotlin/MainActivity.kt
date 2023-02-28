5891bb334601aff2bc5430af63624b8f2d4d85da
Ichsanul Kamil S <ichkmlsdr@gmail.com>
2023-02-28 16:03:25 +0700

    update from video ref

--- app/src/main/java/com/kamil/dialogflowandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/kamil/dialogflowandroidkotlin/MainActivity.kt
@@ -2,12 +2,22 @@ package com.kamil.dialogflowandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.api.gax.core.FixedCredentialsProvider
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.cloud.dialogflow.v2.*
 import com.kamil.dialogflowandroidkotlin.adapters.ChatAdapter
 import com.kamil.dialogflowandroidkotlin.models.Message
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
     private var messageList: ArrayList<Message> = ArrayList()
@@ -15,13 +25,18 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "mainactivity"
     private lateinit var chatAdapter: ChatAdapter
 
+    // dialogflow
+    private var sessionsClient: SessionsClient? = null
+    private var sessionName: SessionName? = null
+    private val uuid = UUID.randomUUID().toString()
+
+    private val chatView: RecyclerView = findViewById(R.id.chatView)
+    private val btnSend: ImageButton = findViewById(R.id.btnSend)
+    private val editMessage: EditText = findViewById(R.id.editMessage)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val chatView: RecyclerView = findViewById(R.id.chatView)
-        val btnSend: ImageButton = findViewById(R.id.btnSend)
-        val editMessage: EditText = findViewById(R.id.editMessage)
 
         // setting adapter to recyclerview
         chatAdapter = ChatAdapter(this, messageList)
@@ -37,17 +52,74 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please enter text!", Toast.LENGTH_SHORT).show()
             }
         }
+
+        // initial bot config
+        setUpBot()
     }
 
     private fun sendMessageToList(message: String) {
         // send message to the bot
+        val input = QueryInput.newBuilder()
+            .setText(TextInput.newBuilder().setText(message).setLanguageCode("en-US")).build()
+
+        GlobalScope.launch {
+            sendMessageInBg(input)
+        }
+    }
+
+    private suspend fun sendMessageInBg(queryInput: QueryInput){
+        withContext(Default){
+            try {
+                val detectIntentRequest = DetectIntentRequest.newBuilder()
+                    .setSession(sessionName.toString())
+                    .setQueryInput(queryInput)
+                    .build()
+                val result = sessionsClient?.detectIntent(detectIntentRequest)
+                if (result != null){
+                    runOnUiThread{
+                        updateIU(result)
+                    }
+                }
+            }catch (e: java.lang.Exception){
+                Log.d(TAG, "doInBackground: "+ e.message)
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun updateIU(response: DetectIntentResponse){
+        val botReply: String = response.queryResult.fulfillmentText
+        if (botReply.isNotEmpty()){
+            addMessageToList(botReply, true)
+        }else{
+            Toast.makeText(this, "some went wrong", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setUpBot(){
-
+        // initialize the dialogflow
+        try{
+            val stream = this.resources.openRawResource(R.raw.credential)
+            val credentials: GoogleCredentials = GoogleCredentials.fromStream(stream)
+                .createScoped("https://www.googleapis.com/auth/cloud-platform")
+            val projectId: String = (credentials as ServiceAccountCredentials).projectId
+            val settingsBuilder: SessionsSettings.Builder = SessionsSettings.newBuilder()
+            val sessionsSettings: SessionsSettings = settingsBuilder.setCredentialsProvider(
+                FixedCredentialsProvider.create(credentials)
+            ).build()
+            sessionsClient = SessionsClient.create(sessionsSettings)
+            sessionName = SessionName.of(projectId,uuid)
+            Log.d(TAG, "projectId: $projectId")
+        }catch (e: java.lang.Exception){
+            Log.d(TAG, "setUpBot: setUpBpt: "+e.message)
+        }
     }
 
     private fun addMessageToList(message: String, isReceived: Boolean) {
         // handle UI change
+        messageList.add(Message(message, isReceived))
+        editMessage.setText("")
+        chatAdapter.notifyDataSetChanged()
+        chatView.layoutManager?.scrollToPosition(messageList.size - 1)
     }
 }
